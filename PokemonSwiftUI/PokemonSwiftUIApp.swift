9c75037d0b000a5d93ff7c255277f3b7e1781fe1
Giuliano Soria <44099099+GiulianoSoria@users.noreply.github.com>
2021-05-29 17:53:32 -0700

    Initial Commit

---
+++ PokemonSwiftUI/PokemonSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonSwiftUIApp.swift
+//  PokemonSwiftUI
+//
+//  Created by Giuliano Soria Pazos on 2021-05-29.
+//
+
+import SwiftUI
+
+@main
+struct PokemonSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
